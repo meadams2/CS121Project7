e02005b65660f0264ec4e5141181e62b7b7c027f
Marianne Adams <meadams2@bsu.edu>
2025-02-24 21:23:43 +0000

    Initialize main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+// main.cpp
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "date.h"
+#include "address.h"
+#include "student.h"
+
+void printStudents(std::vector students);
+void showStudentNames(std::vector students);
+void findStudent(std::vector students);
+void delStudents(std::vector students);
+std::string menu();
+
+int main(){
+	std::cout << "Hello!" << std::endl;
+
+} //end main
