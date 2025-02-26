80468aef0e0f2fe468fa7ebb137e62683b18b81f
Marianne Adams <meadams2@bsu.edu>
2025-02-26 15:50:25 +0000

    End of class 2/26

--- main.cpp
+++ main.cpp
@@ -15,6 +15,45 @@ void delStudents(std::vector students);
 std::string menu();
 
 int main(){
-	std::cout << "Hello!" << std::endl;
+	std::vector<Student> students;
+	loadStudents(students);
+	bool keepGoing = true;
+
+	while (keepGoing){
+		menu();
+
+		if (menu() == 0){
+			delStudents(students);
+			keepGoing = false;
+		} //End quit condition
+		
+		else if (menu() == 1){
+			showStudentNames(students);
+		} //End studentNames condition
+
+		else if (menu() == 2){
+			printStudents(students);
+		} //End print students condition
+
+		else if (menu() == 3){
+			findStudent(students);
+		} //End find student condition
+
+	} //end while loop
 
 } //end main
+
+std::string menu(){
+	std::string userInput;
+
+	std::cout << "0) Quit" << std::endl;
+	std::cout << "1) Print all student names" << std::endl;
+	std::cout << "2) Print all student data" << std::endl;
+	std::cout << "3) Find a student" << std::endl;
+
+	std::cout << "Please choose 0-3: ";
+	std::cin >> userInput;
+
+	return userInput;
+} //end menu()
+
